607564b1555bbc396564bec6cbe67e03d19a4719
Arjun M <90825849+Arjun8420@users.noreply.github.com>
2021-11-17 11:53:50 +0530

    Add files via upload

---
+++ 2D transpose.py
@@ -0,0 +1,12 @@
+# To find the 2D transpose of a matrix
+
+import numpy as np
+
+a = np.array([[1,2,3,4],
+               [5,6,7,8]])
+
+#After transpose the matrix rows and column changes
+print(a.transpose())
+
+
+
